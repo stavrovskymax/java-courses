4856b9f8d65871074b8f7ea49ffcecba19d66cbe
stavrovskymax <stavrovsky96@yandex.ru>
2018-07-08 23:23:04 +0300

    create calculate

--- calculate.java
+++ calculate.java
@@ -3,5 +3,9 @@ public class calculate
  public static void main(String[] arg)
  {
   System.out.println("Hello world");
+  int a = Integer.valueOf(arg[0]);
+  int b = Integer.valueOf(arg[1]);
+  int sum = a + b;
+  System.out.println("Sum = " + sum);
  }
 }
