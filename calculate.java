8896127fd7ff6a8ede9963ca3183c1825e19d718
stavrovskymax <stavrovsky96@yandex.ru>
2018-07-12 21:50:11 +0300

    2 verdion

--- calculate.java
+++ calculate.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class calculate
 {
  public static void main(String[] arg)
  {
-  System.out.println("Hello world");
-  int a = Integer.valueOf(arg[0]);
-  int b = Integer.valueOf(arg[1]);
-  int sum = a + b;
-  System.out.println("Sum = " + sum);
+  System.out.println("Выберите действие");
+  Scanner in = new Scanner(System.in);
+  String s = in.nextLine(); 
+  double sum = 0;
+  double a = in.nextDouble();
+  double b = in.nextDouble();
+  char ss = s.charAt(0);
+  if (ss == '+') {
+     sum = a + b;
+  }
+  if (ss == '-') {
+     sum = a - b;
+  }
+  if (ss == '*') {
+     sum = a * b;
+  }
+  if (ss == '/') {
+     sum = a / b;
+  }  
+  System.out.println("Sum = " + sum); 
  }
 }
