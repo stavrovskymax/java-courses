5284f4c4e576efe3f0d83c5cc50c5e7eb4b1bdd3
stavrovskymax <stavrovsky96@yandex.ru>
2018-08-22 00:24:56 +0300

    Find index, replace element and triangle array

--- HexletArray.java
+++ HexletArray.java
@@ -1,9 +1,14 @@
 class Loops {
-    int a[];
-    int num;
-    int idx;
+    private int a[];
+    private int idx;
     
-    int findIndexArray(int a[], int num) {
+    Loops(int arrayLoops[]) {
+        this.a = arrayLoops;
+        this.idx = 0;
+    }
+    
+    // Поиск элемента в массиве
+    int findIndexArray(int num) {
         for(int i=0; i < a.length; i++) {
             if(a[i] == num) {
                 idx = i;
@@ -15,27 +20,82 @@ class Loops {
         return idx;
     }
 
-    void replaceElement(int a[], int b, int c) {
+    // Замена элементов массива
+    void replaceElement(int b, int c) {
        for(int i=0; i < a.length; i++) {
            if(a[i] == b) a[i] = c;
        }
    }
+    
+    // Создать треугольный массив
+    void triangleArray(int numArray) {
+        if (numArray < 2) {
+                System.out.print("null");
+                return;
+            }
+        int numbers[][] = new int[numArray][];
+        System.out.println("arg[0][0] = null");
+        for(int i=0; i < numArray; i++) {
+            numbers[i] = new int[i];
+            for(int j=0; j < numbers[i].length; j++) {
+                numbers[i][j] = i;
+                System.out.println("arg[" + i + "][" + j + "] = " + numbers[i][j]);
+            }
+        }
+    }
 }
 
 class HexletArray {
     public static void main(String args[]) {
-        Loops metod = new Loops();
         
-        int array[] = {5, 6, 4, 5};
+        final int array[] = {5, 6, 4, 5};        
         
-/*        int index = 4;
+        Loops metod = new Loops(array);
         
-        System.out.println(metod.findIndex(array, index)); */
-        
-        metod.replaceElement(array, 5, 10);
+        System.out.println("Source array");
         
         for(int i=0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        
+        System.out.println("\n");
+            
+        System.out.println("Searching for an element in an array");
+        
+        final int index = 4;
+        
+        System.out.println("Find element by index: " + index);
+        
+        System.out.println("Result: " + metod.findIndexArray(index));
+        
+        System.out.println();
+        
+        System.out.println("Replacing array elements");
+        
+        final int inReplace = 5; 
+        
+        final int outReplace = 10;
+        
+        System.out.println("Replace " + inReplace + " to " + outReplace);
+        
+        metod.replaceElement(inReplace, outReplace);
+        
+        System.out.print("Result:");
+        
+        for(int i=0; i < array.length; i++) {
+            System.out.print(" " + array[i]);
+        }
+        
+        System.out.println("\n");
+        
+        System.out.println("Create a triangle array");
+        
+        final int sizeTriangleArray = 4;
+        
+        System.out.println("Size of a triangle array: " + sizeTriangleArray);
+        
+        System.out.println("Result:");
+        
+        metod.triangleArray(sizeTriangleArray);
     }
 }
